cd5b6b620d7ef24449ef6e29b677e413155a5560
safecyber <152196871+safecyber@users.noreply.github.com>
2025-02-02 20:18:38 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        } else {
+            window.location.href = this.getAttribute('href');
+        }
+    });
+});
